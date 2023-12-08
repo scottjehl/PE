5090186dc7b7f2067f9ef5c15956c9920828723e
Scott Jehl <scottjehl@users.noreply.github.com>
2023-12-08 10:42:01 -0500

    Update _worker.js

--- demo/_worker.js
+++ demo/_worker.js
@@ -12,20 +12,37 @@ export default {
       			]
       	};
       
-        const html = `yay`;
+        const html = `<!DOCTYPE html>
+        <html>
+        <body>
+        <h1 data-pe-text="store.title">${store.title}</h1>
+        <ul>
+          ${store.listitems.map((item,i) => `
+            <li data-pe-each="store.listitems" data-pe-text="store.listitems[${i}].text">${item.text}</li>
+          `).join('')}
+        </ul>
+        
+        <script type="module">
+          import pe from 'pe.js';
+          const store = JSON.parse('${JSON.stringify(store)}');
+          // pass the store to a new PE instance
+          const PE = new pe(store, "store");
+          window.store = PE.data;
+        </script>
+        </body>
+        </html>
+        `;
       
         return new Response(html, {
           headers: {
             "content-type": "text/html;charset=UTF-8",
           },
         });
-
-      
     }
     // Otherwise, serve the static assets.
     // Without this, the Worker will error and no assets will be served.
     return env.ASSETS.fetch(request);
-  },
+  }
 }
   
 
