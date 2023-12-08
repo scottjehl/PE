c6b70f53edab548adb105e9a20bec492bbf52f37
Scott Jehl <scottjehl@users.noreply.github.com>
2023-12-08 10:33:06 -0500

    Update _worker.js

--- demo/_worker.js
+++ demo/_worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    if (url.pathname === '/' ) {
+    if (url.pathname === '' ) {
       
 
       	const store = { 
