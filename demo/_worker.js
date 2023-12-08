a5160e5d51d3d0c72e565ea93484348316f9f781
Scott Jehl <scottjehl@users.noreply.github.com>
2023-12-08 10:31:13 -0500

    Create _worker.js

---
+++ demo/_worker.js
@@ -0,0 +1,46 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    if (url.pathname.startsWith('/index')) {
+      
+
+      	const store = { 
+      			title: "My List",
+      			listitems: [
+      				{text: "here's a list item"}, 
+      				{text: "here's another one"},
+      			]
+      	}
+        const html = `<!DOCTYPE html>
+        <body>
+        <h1 data-pe-text="store.title">${store.title}</h1>
+        <ul>
+          ${store.listitems.map((item,i) => `
+            <li data-pe-each="store.listitems" data-pe-text="store.listitems[${i}].text">${item.text}</li>
+          `).join('')}
+        </ul>
+        <script type="module">
+          import pe from '/pe.js';
+          const store = JSON.parse('${JSON.stringify(store)}');
+          // pass the store to a new PE instance
+          const PE = new pe(store, "store");
+          window.store = PE.data;
+        </script>
+        </body>
+        </html>	
+        `;
+        return new Response(html, {
+          headers: {
+            "content-type": "text/html;charset=UTF-8",
+          },
+        });
+
+      
+    }
+    // Otherwise, serve the static assets.
+    // Without this, the Worker will error and no assets will be served.
+    return env.ASSETS.fetch(request);
+  },
+}
+  
+
